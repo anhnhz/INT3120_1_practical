40ad7bb8723bf940488f74443ce7e77e7c762b34
anhnhz <leduchoanganh205@gmail.com>
2025-10-14 23:54:36 +0700

    Unit5_Pathway2_Add-Repository

---
+++ Unit5_Pathway2_Add-Repository/basic-android-kotlin-compose-training-mars-photos/app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
@@ -0,0 +1,11 @@
+import com.example.marsphotos.model.MarsPhoto
+import com.example.marsphotos.network.MarsApiService
+
+interface MarsPhotosRepository {
+    suspend fun getMarsPhotos(): List<MarsPhoto>
+}
+
+class NetworkMarsPhotosRepository
+    (private val marsApiService: MarsApiService) : MarsPhotosRepository {
+    override suspend fun getMarsPhotos(): List<MarsPhoto> = marsApiService.getPhotos()
+}
---
+++ Unit5_Pathway2_Add-Repository/basic-android-kotlin-compose-training-mars-photos/app/src/test/java/com/example/marsphotos/fake/FakeDataSource.kt
@@ -0,0 +1,21 @@
+import com.example.marsphotos.model.MarsPhoto
+import com.example.marsphotos.network.MarsApiService
+
+object FakeDataSource {
+
+    const val idOne = "img1"
+    const val idTwo = "img2"
+    const val imgOne = "url.1"
+    const val imgTwo = "url.2"
+    val photosList = listOf(
+        MarsPhoto(
+            id = idOne,
+            imgSrc = imgOne
+        ),
+        MarsPhoto(
+            id = idTwo,
+            imgSrc = imgTwo
+        )
+    )
+}
+
